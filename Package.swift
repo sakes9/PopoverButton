aaa73fe418a4f4f532bdd4da6e1e1c4ad1c877d4
sake <sake.std.co@gmail.com>
2024-08-03 20:24:13 +0900

    chore: Specify iOS v17 as the platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PopoverButton",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,5 +22,4 @@ let package = Package(
         .testTarget(
             name: "PopoverButtonTests",
             dependencies: ["PopoverButton"]),
-    ]
-)
+    ])
